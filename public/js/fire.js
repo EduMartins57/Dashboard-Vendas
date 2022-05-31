ea7186db6fef85161110fe550f17fcb4f123e381
Eduardo Martins <druhenrique57@gmail.com>
2022-05-31 18:48:00 -0300

    atualização

--- public/js/fire.js
+++ public/js/fire.js
@@ -1,20 +1,21 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAlg2DHGI3cSAZ092ZI_WEQy6EnrPz8x_U",
-    authDomain: "dashboard-vendas-38027.firebaseapp.com",
-    projectId: "dashboard-vendas-38027",
-    storageBucket: "dashboard-vendas-38027.appspot.com",
-    messagingSenderId: "260328449489",
-    appId: "1:260328449489:web:c28afb9ed57439ee5aecf0",
-    measurementId: "G-VEMMP140LH"
-  };
+  apiKey: "AIzaSyAlg2DHGI3cSAZ092ZI_WEQy6EnrPz8x_U",
+  authDomain: "dashboard-vendas-38027.firebaseapp.com",
+  databaseURL: "https://dashboard-vendas-38027-default-rtdb.firebaseio.com",
+  projectId: "dashboard-vendas-38027",
+  storageBucket: "dashboard-vendas-38027.appspot.com",
+  messagingSenderId: "260328449489",
+  appId: "1:260328449489:web:c28afb9ed57439ee5aecf0",
+  measurementId: "G-VEMMP140LH"
+};
 
-  firebase.initializeApp(firebaseConfig)
+  const app = initializeApp(firebaseConfig)
 
   let imagemSelecionada;
 
   let tabela = document.getElementById('tabelaCategoria').getElementsByTagName("tbody")[0]
 
-  let bd = firebase.firestore();
+  let bd = firebase.getFirestore();
 
  // singleEventListener (estoque) 
   bd.collection("estoque").doc("1").get().then(function (doc){
